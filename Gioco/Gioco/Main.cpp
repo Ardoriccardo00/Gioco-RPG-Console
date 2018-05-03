007903f9a545f2f9bcad6759a1bd9538d26bff04
riccardo colombo <richi00@libero.it>
2018-05-03 17:28:14 +0200

    terzo aggiornamento del 03/05/2018 -Migliorate molte cose; -Aggiunte le funzioni Posizione, Esamina, statistiche, Mappa.

--- Gioco/Gioco/Main.cpp
+++ Gioco/Gioco/Main.cpp
@@ -9,6 +9,7 @@
 #include "Stringhe.h"
 #include "Menu.h"
 #include "Personaggio.h"
+#include "Gioco.h"
 using namespace::std;
 
 int main() {
@@ -20,14 +21,14 @@ int main() {
 	int salute = 0;
 	int exp = 0;
 	int scelta;
+	int posizione = 1;
 	Menu m;
 	Personaggio p;
+	Gioco g;
 	m.CreaMenu();
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hConsole, 15);
-	int a;
-	cin >> a;
-	if (a == 1) { 
+	_getch();
 		cout << "Inizio gioco";
 		Sleep(500);
 		cout << "\nCaricamento ";
@@ -42,42 +43,56 @@ int main() {
 
 		p.CreaPersonaggio(nome, sprite, livello , attacco, difesa, salute, exp);
 
-		cout << "\nBene, ora che hai creato un personaggio inizia il gioco";
-		Sleep(5000);
+		cout << "\nBene, ora che hai creato un personaggio, premi invio per iniziare il gioco";
+		_getch();
+		Sleep(1000);
 		for (int i = 0; i < 100; i++) {
-			SetConsoleTextAttribute(hConsole, 4);
+			SetConsoleTextAttribute(hConsole, 12);
 			cout << "01010001011101010110010101110011011101000110000100100000111010000010000001110101011011100110000100100000011100110110001101110010011010010111010001110100011000010010000001101001011011100010000001100010011010010110111001100001011100100110100101101111001000000111000001100101";
 		}
 		system("cls");
 		Sleep(1000);
-		cout << "help";
-		Sleep(20);
+		cout << "help" << endl;
+		Sleep(40);
 		system("cls");
-		Sleep(5000);
-		SetConsoleTextAttribute(hConsole, 15);
+		Sleep(1000);
+		SetConsoleTextAttribute(hConsole, 11);
 		do {
 		
-		  
-			cout << "\n##############################################################################";
-			cout << "\n#############################Menu' avventura##################################";
-			cout << "\n##############################################################################";
-			cout << "\nSpostati";
-			cout << "\n##############################################################################";
-			cout << "\nEsamina";
-			cout << "\n##############################################################################";
-			cout << "\nStatistiche";
-			cout << "\n##############################################################################";
-			cout << "\n";
-		    
+			SetConsoleTextAttribute(hConsole, 11);
+			cout << "##############################################################################" << endl;
+			cout << "#############################";
+			SetConsoleTextAttribute(hConsole, 14);
+			cout << "Menu' avventura";
+			SetConsoleTextAttribute(hConsole, 11);
+            cout << "##################################" << endl;
+			cout << "##############################################################################" << endl;
+			SetConsoleTextAttribute(hConsole, 12);
+			cout << "Spostati" << endl;
+			SetConsoleTextAttribute(hConsole, 11);
+			cout << "##############################################################################" << endl;
+			SetConsoleTextAttribute(hConsole, 8);
+			cout << "Esamina" << endl;
+			SetConsoleTextAttribute(hConsole, 11);
+			cout << "##############################################################################" << endl;
+			SetConsoleTextAttribute(hConsole, 9);
+			cout << "Statistiche" << endl;
+			SetConsoleTextAttribute(hConsole, 11);
+			cout << "##############################################################################" << endl;
+			SetConsoleTextAttribute(hConsole, 10);
+			cout << "Mappa" << endl;
+			SetConsoleTextAttribute(hConsole, 11);
+			cout << "##############################################################################" << endl;
+
 			cin >> scelta;
 
-			if (scelta == 1) {/*Spostati*/}
-			else if (scelta == 2) {/*Esamina*/}
-			else if (scelta == 3) { p.Statistiche(); }
+			if (scelta == 1) { SetConsoleTextAttribute(hConsole, 12); cout << "inserisci una posizione: " << endl; cin >> posizione; }
+			else if (scelta == 2) { SetConsoleTextAttribute(hConsole, 8);  g.Esamina();        }
+			else if (scelta == 3) { SetConsoleTextAttribute(hConsole, 9);  p.Statistiche();    }
+			else if (scelta == 4) { SetConsoleTextAttribute(hConsole, 10); g.Mappa(posizione); }
 			_getch();
 		} while (true);
 
-	}
 	_getch();
 
 	return 0;
