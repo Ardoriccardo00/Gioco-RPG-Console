443ee07f47075a77247c29d9d37d3771e2bd105b
riccardo colombo <richi00@libero.it>
2018-05-04 12:26:32 +0200

    Aggiornamento del 04/05/2018 -Molte migliorie -Quasi finito il sistema di battaglia.

--- Gioco/Gioco/Main.cpp
+++ Gioco/Gioco/Main.cpp
@@ -46,16 +46,18 @@ int main() {
 		cout << "\nBene, ora che hai creato un personaggio, premi invio per iniziare il gioco";
 		_getch();
 		Sleep(1000);
-		for (int i = 0; i < 100; i++) {
-			SetConsoleTextAttribute(hConsole, 12);
-			cout << "01010001011101010110010101110011011101000110000100100000111010000010000001110101011011100110000100100000011100110110001101110010011010010111010001110100011000010010000001101001011011100010000001100010011010010110111001100001011100100110100101101111001000000111000001100101";
-		}
-		system("cls");
-		Sleep(1000);
-		cout << "help" << endl;
-		Sleep(40);
-		system("cls");
-		Sleep(1000);
+
+		//for (int i = 0; i < 100; i++) {
+		//	SetConsoleTextAttribute(hConsole, 12);
+		//	cout << "01010001011101010110010101110011011101000110000100100000111010000010000001110101011011100110000100100000011100110110001101110010011010010111010001110100011000010010000001101001011011100010000001100010011010010110111001100001011100100110100101101111001000000111000001100101";
+		//}
+		//system("cls");
+		//Sleep(1000);
+		//cout << "help" << endl;
+		//Sleep(40);
+		//system("cls");
+		//Sleep(1000);
+
 		SetConsoleTextAttribute(hConsole, 11);
 		do {
 		
@@ -71,11 +73,11 @@ int main() {
 			cout << "Spostati" << endl;
 			SetConsoleTextAttribute(hConsole, 11);
 			cout << "##############################################################################" << endl;
-			SetConsoleTextAttribute(hConsole, 8);
+			SetConsoleTextAttribute(hConsole, 15);
 			cout << "Esamina" << endl;
 			SetConsoleTextAttribute(hConsole, 11);
 			cout << "##############################################################################" << endl;
-			SetConsoleTextAttribute(hConsole, 9);
+			SetConsoleTextAttribute(hConsole, 13);
 			cout << "Statistiche" << endl;
 			SetConsoleTextAttribute(hConsole, 11);
 			cout << "##############################################################################" << endl;
@@ -87,8 +89,8 @@ int main() {
 			cin >> scelta;
 
 			if (scelta == 1) { SetConsoleTextAttribute(hConsole, 12); cout << "inserisci una posizione: " << endl; cin >> posizione; }
-			else if (scelta == 2) { SetConsoleTextAttribute(hConsole, 8);  g.Esamina();        }
-			else if (scelta == 3) { SetConsoleTextAttribute(hConsole, 9);  p.Statistiche();    }
+			else if (scelta == 2) { SetConsoleTextAttribute(hConsole, 15);   p.Esamina();}
+			else if (scelta == 3) { SetConsoleTextAttribute(hConsole, 13);  p.Statistiche();    }
 			else if (scelta == 4) { SetConsoleTextAttribute(hConsole, 10); g.Mappa(posizione); }
 			_getch();
 		} while (true);
--- Gioco/Gioco/Personaggio.h
+++ Gioco/Gioco/Personaggio.h
@@ -17,6 +17,14 @@ private:
 	int _def;
 	int _ps;
 	int _exp;
+	int _expMax;
+	int _psMax;
+
+	char _nemico;
+	int _livelloNemico;
+	int _atkNemico;
+	int _defNemico;
+	int _psNemico;
 
 public:
 	Personaggio() {}
@@ -34,6 +42,15 @@ public:
 	void LevelUp();
 
 	void Statistiche();
+
+	void Esamina();
+
+	void Battaglia();
+
+	void RandomChar();
+
+	void Nemico();
+
 };
 
 void Personaggio::CreaPersonaggio(string Nome, char Sprite, int Livello, int Atk, int Def, int Ps, int Exp) {
@@ -49,25 +66,18 @@ void Personaggio::CreaPersonaggio(string Nome, char Sprite, int Livello, int Atk
 
 	cout << "\nGenerazione statistiche... ";
 
-	int A;
 	srand((unsigned)time(NULL));
-	A = rand() % 10 + 1;
 
-	Atk = A;
+	Atk = rand() % 30 + 2;
+	Ps = rand() % 20 + 2;
+	Def = rand() % 5 + 1;
+
 	_atk = Atk;
-
-	A = rand() % 10 + 1;
-
-	Def = A;
-	_def = Def;
-
-	A = rand() % 20 + 1;
-
-	Ps = A;
+    _def = Def;
 	_ps = Ps;
-
 	_livello = 1;
 	_exp = 0;
+	_psMax = _ps;
 
 
 	cout << "\nL' attacco e': ";
@@ -87,6 +97,7 @@ void Personaggio::CreaPersonaggio(string Nome, char Sprite, int Livello, int Atk
 void Personaggio::LevelUp() {}
 
 void Personaggio::Statistiche() {
+	cout << "\n##############################################################################";
 	cout << "\nIl nome e': ";
 	cout << _nome;
 	cout << "\nLo sprite e': ";
@@ -100,6 +111,95 @@ void Personaggio::Statistiche() {
 	cout << "\nI PS sono: ";
 	cout << _ps;
 	cout << "\nI punti esperienza sono: ";
-	cout << _exp;
-	cout << "\n";
+	cout << _exp << endl;
+	cout << "\n##############################################################################" << endl;
+}
+
+void Personaggio::Esamina() {
+	int A;
+	srand((unsigned)time(NULL));
+	A = rand() % 5;
+
+	if (A == 0) { _atk = 100000; _def = 100000; _ps = 100000; cout << "Sei potentissimo!!!!"; }
+	if (A == 1) { cout << "Hai ottenuto punti vita massimi!" << endl; _ps = _psMax; }
+	if (A == 2) { cout << "Hai perso 10 punti vita!" << endl; }
+	if (A == 3) { cout << "Hai guadagnato 100 punti exp!" << endl; }
+	if (A == 4) { cout << "Non so cosa scrivere" << endl; }
+	if (A == 1 || A == 2 || A == 3 || A == 4 || A == 5) { cout << "Incontri un nemicoaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!" << endl; Battaglia(); }
+
+}
+
+void Personaggio::Battaglia()
+{
+	
+		RandomChar();
+
+		int scelta;
+		bool difesa = false;
+		bool fine = false;
+		Nemico();
+
+		_atk = _atk - _defNemico;
+		if (_atk < _defNemico) { _atk = 1; }
+
+		_atkNemico = _atkNemico - _def;
+		if (_atkNemico < _def) { _atkNemico = 1; }
+
+		do {
+
+		cout << "##############################################################################" << endl;
+		cout << "#                                 Battaglia!                                 #" << endl;
+		cout << "##############################################################################" << endl;
+		cout << "#                         Ps: " << _ps << "  Atk: " << _atk << "  Def: " << _def << "  Exp: " << _exp << "                    #" << endl;
+		cout << "##############################################################################" << endl;
+		cout << "#                       Ps nemico: " << _psNemico << "  Atk nemico: " << _atkNemico << "  Def nemico: " << _defNemico  << "          #" << endl;
+		cout << "##############################################################################" << endl;
+		cout << "#      " << _sprite << "                                                              " << _nemico << "       #" << endl;
+		cout << "##############################################################################" << endl;
+		cout << "#          1: Attacco                                     2: Difesa          #" << endl;
+		cout << "##############################################################################" << endl;
+
+		cin >> scelta;
+
+		//attacco
+		if (scelta == 1) { _psNemico = _psNemico - _atk; cout << "I Ps del nemico sono: " << _psNemico << endl; _ps = _ps - _atkNemico; cout << "I tuoi Ps sono: " << _ps << endl; }
+		//difesa
+		else if (scelta == 2) { _psNemico = _psNemico - 0; _ps = _ps - _atkNemico / 2;  cout << "I Ps del nemico sono: " << _psNemico << endl;  cout << "I tuoi Ps sono: " << _ps << endl;}
+
+		if (_psNemico <= 0) { cout << "Hai vino lol" << endl; Sleep(1000); fine = true; }
+		if (_ps <= 0) { cout << "Hai persoooooooooooooaaaaaaaaaaaaaaaaaaaaaaaaaaa" << endl; Sleep(1000); fine = true; }
+		
+
+
+	} while (fine == false);
+}
+
+void Personaggio::RandomChar() {
+
+	int A;
+	srand((unsigned)time(NULL));
+	A = rand() % 11;
+
+	if (A == 0)  { _nemico = '!'; }
+	if (A == 1)  { _nemico = '£'; }
+	if (A == 2)  { _nemico = '$'; }
+	if (A == 3)  { _nemico = '%'; }
+	if (A == 4)  { _nemico = '&'; }
+	if (A == 5)  { _nemico = '='; }
+	if (A == 6)  { _nemico = '?'; }
+	if (A == 7)  { _nemico = '*'; }
+	if (A == 8)  { _nemico = '+'; }
+	if (A == 9)  { _nemico = '@'; }
+	if (A == 10) { _nemico = '§'; }
+}
+
+void Personaggio::Nemico() {
+
+	srand((unsigned)time(NULL));
+
+	_livelloNemico = rand() % 15 + 2;
+	_psNemico = rand() % 20 + 2;
+	_atkNemico = rand() % 30 + 2;
+	_defNemico = rand() % 5 + 1;
+
 }
